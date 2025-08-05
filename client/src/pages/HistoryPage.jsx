ede25ce46091c9a7dc88f7443ee1edbc3c767576
Rudraksh Sharma <rudraksh2233rr@gmail.com>
2025-08-05 08:17:38 -0700

    remaining deployed url changes in frontend

--- client/src/pages/HistoryPage.jsx
+++ client/src/pages/HistoryPage.jsx
@@ -14,7 +14,7 @@ const HistoryPage = () => {
     const fetchUrls = async () => {
       try {
         setLoading(true);
-        const res = await axios.get(`http://localhost:5000/user/urls?page=${page}&limit=${limit}`, {
+        const res = await axios.get(`https://url-shortner-xclh.onrender.com/user/urls?page=${page}&limit=${limit}`, {
           headers: {
             Authorization: `Bearer ${token}`,
           },
